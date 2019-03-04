db747e83c1a1e4bf524239f224641ab26a5e6450
Jay kamble <jay.v.kamble@gmail.com>
2019-03-04 12:44:47 +0530

    More string methods and how to use method

---
+++ Training_Day1/Methods.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day1
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2(13, 17);
+            int result = Method3(12, 14);
+            Console.WriteLine("Sum = {0}", result);
+            Method4();
+            Method5();
+            Console.Read();
+
+        }
+
+        /// <summary>
+        /// Static methods as we need to call hte methods in the same class
+        /// </summary>
+        static void Method1()
+        {
+            Console.WriteLine("Method1 is executed");
+        }
+
+        static void Method2(int num1, int num2)
+        {
+            Console.WriteLine("Method2 is executed with parameters : {0} and {1} ", num1, num2);
+        }
+
+        static int Method3(int v1, int v2)
+        {
+            int sum = v1 + v2;
+            return sum;
+        }
+
+        static void Method4()
+        {
+            Console.WriteLine("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 * num2);
+        }
+
+        /// <summary>
+        /// We can also call one method inside another method.... Called nested methods
+        /// </summary>
+        static void Method5()
+        {
+            Method1();
+        }
+
+    }
+}
--- Training_Day1/StringMethods.cs
+++ Training_Day1/StringMethods.cs
@@ -13,6 +13,7 @@ namespace Training_Day1
             string str1 = "Hello World";
             string str2 = "hello World";
 
+            //String Compare
             if (str1 == str2)
             {
                 Console.WriteLine("Strings are Equal");
@@ -25,6 +26,7 @@ namespace Training_Day1
             Console.WriteLine("String in Uppercase: " + str1.ToUpper());
             Console.WriteLine("String in Lowercase: " + str2.ToLower());
 
+            //String compare using Equals
             if (str1.Equals(str2))
             {
                 Console.WriteLine("Strings are Equal using Equals()");
@@ -33,6 +35,31 @@ namespace Training_Day1
             {
                 Console.WriteLine("Strings are not Equal using Equals()");
             }
+
+            //Convert to string
+            int price = 50;
+            string strprice = price.ToString();
+
+            //Padding
+            string language = "Python";
+            language = language.PadLeft(10, '#');
+            Console.WriteLine(language);
+
+            //Splitting string
+            string langNames = "Java|C#|Typescript|F#|JQuery|C|C++";
+            string[] names = langNames.Split('|');
+            foreach (string lan in names)
+            {
+                Console.WriteLine(lan);
+            }
+
+            //Joining Strings
+            string n1 = "Hello, ";
+            string n2 = "Welcome to ";
+            string n3 = "string tutorial";
+
+            string join = string.Join("", n1, n2, n3);
+            Console.WriteLine(join);
 
             Console.Read();
         }
