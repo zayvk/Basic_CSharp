71d663c6f8e933673008ff10de4d6bf93c18fee2
Jay kamble <jay.v.kamble@gmail.com>
2019-03-05 12:02:51 +0530

    Enums and Structs

---
+++ Training_Day2/Structures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day2
+{
+    /// <summary>
+    /// Structure cannot have parameterless constructor
+    /// structures are value types whereas classes are ref types
+    /// Structures can not have inheritance but they can implement interfaces
+    /// </summary>
+    struct Book
+    {
+        public string bookName;
+        public int noOfpages;
+
+        public void ReadBook()
+        {
+            Console.WriteLine("reading the Book");
+        }
+
+        public Book(string name, int np)
+        {
+            bookName = name;
+            noOfpages = np;
+        }
+    }
+    class Structures
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", 519);
+            book.ReadBook();
+            Console.WriteLine(book.bookName + " " + book.noOfpages);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Training_Day2/usingEnums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day2
+{
+    enum Status
+    {
+        Permanent,
+        Temporary,
+        Consultant,
+        Trainee
+    }
+
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    class Person
+    {
+        public string name;
+        public Status status;
+        public Gender gender;
+    }
+
+    class UsingEnums
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.name = "Jay";
+            person.status = 0;
+            person.gender = 0;
+
+            Console.WriteLine("employee name is " + person.name + " with status " + person.status + " and gender is " + person.gender);
+            Console.ReadLine();
+        }
+    }
+}
