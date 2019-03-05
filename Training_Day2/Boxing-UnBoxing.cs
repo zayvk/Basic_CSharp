9f8a50edcf8271f68ae1451dca0a3bc2801511c4
Jay kamble <jay.v.kamble@gmail.com>
2019-03-05 11:19:48 +0530

    Conversions, Boxing and Unboxing, Types Updated

---
+++ Training_Day2/Boxing-UnBoxing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day2
+{
+    class Boxing_UnBoxing
+    {
+        static void Main(string[] args)
+        {
+            //Boxing: converting value type to reference types
+            int j = 200;
+            object o = j;  //Implicitli happens
+
+            //Unboxing: converting Reference type to value types
+            object obj = 100;
+            int i = (int)obj; //Explicitly need to do
+        }
+    }
+}
---
+++ Training_Day2/Conversions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Training_Day2
+{
+    class Conversions
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Conversion
+            int i = 100;
+            float f1 = i;
+
+            //Explicit Conversion
+            float f2 = 121.13f;
+            int j = (int)f2;
+
+            Console.WriteLine(f1);
+            Console.WriteLine(j);
+            Console.ReadLine();
+        }
+    }
+}
