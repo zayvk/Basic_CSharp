d682599249ae00382086964812349337ba09c891
Jay kamble <jay.v.kamble@gmail.com>
2019-03-05 13:31:03 +0530

    Oops Concepts and static attribute

---
+++ Training_Day2/OOP/OopsDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day2.OOP
+{
+
+    class Items
+    {
+        public string itemName;
+        int itemId;
+
+        public Items()
+        {
+            Console.WriteLine("Parameterless Constructor");
+        }
+
+        public Items(string itemname, int itemid)
+        {
+            this.itemName = itemname;
+            this.itemId = itemid;
+        }
+
+        public void GetItem()
+        {
+            Console.WriteLine("Name= " + itemName + " and " + itemId);
+        }
+    }
+    class OopsDemo
+    {
+        static void Main(string[] args)
+        {
+            Items item = new Items();
+            Items i = new Items("Demo Item", 101);
+            i.GetItem();
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ Training_Day2/OOP/Static.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day2.OOP
+{
+
+    class MathUtility
+    {
+        public static void Addition()
+        {
+
+        }
+
+        public static void Substract()
+        {
+
+        }
+    }
+
+    class Test
+    {
+        public static int cnt1;
+        public int cnt2;
+
+        public Test()
+        {
+            cnt1++;
+            cnt2++;
+        }
+    }
+
+    class Static
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+
+            Test t1 = new Test();
+            Test t2 = new Test();
+            Test t3 = new Test();
+            Test t4 = new Test();
+            Console.WriteLine(Test.cnt1);
+            Console.WriteLine(t4.cnt2);
+
+            MathUtility.Addition();
+            MathUtility.Substract();
+
+
+            Console.ReadLine();
+        }
+
+        static void Method1()
+        {
+
+        }
+
+        void Method2()
+        {
+             
+        }
+    }
+}
