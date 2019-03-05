9081e81a93b29789a85bc09a8596a79199416aba
Jay kamble <jay.v.kamble@gmail.com>
2019-03-05 10:23:27 +0530

    Custom exception handling

---
+++ Training_Day1/CustomException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Day1
+{
+    class CustomException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                EvenNumberProcessing();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void EvenNumberProcessing()
+        {
+            Console.WriteLine("Enter Even Number: ");
+            int evenNo = Convert.ToInt32(Console.ReadLine());
+
+            if (evenNo%2 != 0)
+            {
+                throw new OddNumberException("The number is Odd number.");
+            }
+            else
+            {
+                Console.WriteLine("It is an even Number.");
+            }
+        }
+    }
+}
---
+++ Training_Day1/OddNumberException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Training_Day1
+{
+    [Serializable]
+    internal class OddNumberException : Exception
+    {
+        public OddNumberException()
+        {
+        }
+
+        public OddNumberException(string message) : base(message)
+        {
+        }
+
+        public OddNumberException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OddNumberException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
