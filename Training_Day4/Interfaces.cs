f71f548aec4cd29ec624199fcf1d41eb5270daf2
Jay kamble <jay.v.kamble@gmail.com>
2019-03-08 09:52:36 +0530

    Interfaces implementation

--- Training_Day4/Interfaces.cs
+++ Training_Day4/Interfaces.cs
@@ -6,16 +6,91 @@ using System.Threading.Tasks;
 
 namespace Training_Day4
 {
-    class Interfaces
+    //interface will have just definitions for
+    //methods
+    //properties
+    //events
+    //indxer
+    //by default all memebers are public
+
+    interface IDataAccess
     {
-        public class FileAccess
+        string ReadData(string connectionName);
+        void writeData(string connectionName);
+        string DataConnection { get; set; }
+    }
+
+    interface IMessaging
+    {
+        void SendSMS();
+        void SendEmail();
+    }
+    class FileAccess : IDataAccess, IMessaging
+    {
+        public string DataConnection { get; set; }
+
+        public string ReadData(string connectionName)
         {
 
+            Console.WriteLine("Reading data from File");
+            return "File data";
         }
 
+        public void SendEmail()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SendSMS()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void writeData(string connectionName)
+        {
+            Console.Write("writing data to File server");
+
+        }
+    }
+    class SQLDataAccess : IDataAccess
+    {
+        public string DataConnection { get; set; }
+
+        public string ReadData(string connectionName)
+        {
+
+            Console.WriteLine("Reading data from SQL Server");
+            return "SQL data";
+        }
+
+        public void writeData(string connectionName)
+        {
+            Console.Write("writing data to SQL server");
+
+        }
+    }
+    class Interfaces
+    {
         static void Main(string[] args)
         {
-            
+            //SQLDataAccess access = new SQLDataAccess();
+            ////IDataAccess iaccess = access;
+            //access.DataConnection = "sql";
+            //SaveData(access);
+
+            FileAccess access = new FileAccess();
+            access.DataConnection = "filename";
+            SaveData(access);
+            //access.ReadData("sql");
+            //access.writeData("sql");
+            Console.ReadLine();
+        }
+
+        static void SaveData(IDataAccess access) //interface
+        {
+            access.ReadData(access.DataConnection);
+            access.writeData(access.DataConnection);
         }
     }
 }
+
